74073d1e42e595f7d1e484fb22cb9d72d6a2921b
Markus Kofler <markkofler@edu.aau.at>
2022-03-03 20:13:49 +0100

    Add inital layout

--- app/src/main/java/com/example/uebung0/MainActivity.java
+++ app/src/main/java/com/example/uebung0/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.uebung0;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickSend(View view) {
+        EditText input = (EditText) findViewById(R.id.editTextMatrikelnummer);
+        String matNum = input.getText().toString();
+
+        Toast.makeText(this, matNum, Toast.LENGTH_LONG).show();
     }
 }
